e2bf54bb614cec4a4b7d361c480f74d3303e7445
Денис <den.kochkin36@mail.ru>
2022-02-20 00:45:39 +0300

    начал делать калькулятор для блока питания

--- scripts/data.js
+++ scripts/data.js
@@ -1,7 +1,79 @@
 const powerUnits = [{
-    "name": "Radeon 1",
-    "power": 1000
+    "name": "Core I9 12900K",
+    "power": 241
 }, {
-    "name": "Radeon 2",
-    "power": 500
-}]
+    "name": "Core I9 12900KF",
+    "power": 241
+},{
+    "name": "Core I9 12900",
+    "power": 202
+},{
+    "name": "Core I9 12900F",
+    "power": 202
+},{
+    "name": "Core I7 12700K",
+    "power": 190
+},{
+    "name": "Core I7 12700",
+    "power": 180
+},{
+    "name": "Core I7 12700F",
+    "power": 180
+},{
+    "name": "Core I7 10900K",
+    "power": 300
+},{
+    "name": "Core I5 12600K",
+    "power": 150
+},{
+    "name": "Core I5 12600KF",
+    "power": 150
+},{
+    "name": "Core I5 12600",
+    "power": 117
+},{
+    "name": "Core I5 12500",
+    "power": 117
+},{
+    "name": "Core I5 12400",
+    "power": 117
+},{
+    "name": "Core I5 12400F",
+    "power": 117
+},{
+    "name": "Core I3 12300",
+    "power": 89
+},{
+    "name": "Core I3 12100",
+    "power": 89
+},{
+    "name": "Core I3 12100F",
+    "power": 89
+},{
+    "name": "Pentium G7400",
+    "power": 46
+},{
+    "name": "Celeron G6900",
+    "power": 46
+},{
+    "name": "Xeon W 1290P",
+    "power": 300
+},{
+    "name": "Core I9 10900KF",
+    "power": 300
+},{
+    "name": "Core I9 10850K",
+    "power": 265
+},{
+    "name": "Core I9 10910",
+    "power": 150
+},{
+    "name": "Xeon W 1290",
+    "power": 80
+},{
+    "name": "Core I9 10900",
+    "power": 300
+},{
+    "name": "Core I9 10900F",
+    "power": 300
+},]
